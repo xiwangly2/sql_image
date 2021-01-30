294c52b2c06b4fcaf389c94301e175b189d1e088
LY <1334850101@qq.com>
2021-01-30 19:19:50 +0800

    Add files via upload

--- images.sql
+++ images.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： localhost
--- 生成日期： 2021-01-23 21:18:48
+-- 生成日期： 2021-01-30 19:00:56
 -- 服务器版本： 5.7.31-log
--- PHP 版本： 7.3.11
+-- PHP 版本： 7.4.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `images` (
-  `name` varchar(255) NOT NULL COMMENT '文件名',
+  `name` varchar(190) NOT NULL COMMENT '文件名',
   `type` varchar(30) NOT NULL COMMENT 'MIME类型',
   `id` int(10) NOT NULL COMMENT 'ID',
   `bin` mediumblob NOT NULL COMMENT '二进制数据'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- 转储表的索引
